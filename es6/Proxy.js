f72b8c39bacf997a235dbf2f79d3ba2feba3b3cd
陈银 <chenyin151@163.com>
2018-03-29 12:53:35 +0800

    1.在对象上直接定义proxy,并且在对象上直接调用proxy 2.Object.create()与new的区别： function Person(){}; var p=new Person(); Object.create(p)与new的区别：Object.create()实例出来的对象 __proto__指向p这个对象，而new出来的对象直接指向Person的prototype

--- es6/Proxy.js
+++ es6/Proxy.js
@@ -24,3 +24,27 @@ var handler = {
 var proxy1 = new Proxy(target, handler);
 proxy1.a = 1;
 console.log('proxy.a', proxy1.a);
+
+// 置到object.proxy属性， 从而可以在object对象上调用。
+var handler1 = {
+    set: function(target, key) {
+        console.log('拦截le')
+    }
+}
+var object = { proxy: new Proxy(target, handler1) };
+console.log(object.proxy.a = 1);
+//------------------------------
+
+// Proxy 实例也可以作为其他对象的原型对象。
+// function Person(){};
+// var p=new Person();
+// Object.create(p)与new的区别：Object.create()实例出来的对象
+// __proto__指向p这个对象，而new出来的对象直接指向Person的prototype
+var proxy = new Proxy({}, {
+    get: function(target, property) {
+        return 35;
+    }
+});
+let obj = Object.create(proxy);
+obj.time;
+//---------------------------------
