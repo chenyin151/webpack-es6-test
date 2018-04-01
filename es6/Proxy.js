26f84bd2953d19052412a0400dc69df0418c179f
陈银 <chenyin151@163.com>
2018-04-02 02:47:38 +0800

    利用get拦截，实现一个生成各种 DOM 节点的通用函数dom

--- es6/Proxy.js
+++ es6/Proxy.js
@@ -155,3 +155,34 @@ var pow = n => n * n;
 var reverseInt = n => n.toString().split("").reverse().join("") | 0;
 console.log('pipe:', pipe(3).double.pow.reverseInt.get);
 // -------------------------------------------------------------
+
+// 下面的例子则是利用get拦截，实现一个生成各种 DOM 节点的通用函数dom
+const dom = new Proxy({}, {
+    get(target, property) {
+        return function(attrs = {}, ...children) {
+            const el = document.createElement(property);
+            for (let prop of Object.keys[attrs]) {
+                el.setAttribute(prop, attrs[prop]);
+            }
+            for (let child of children) {
+                if (typeof child === 'string') {
+                    child = document.createTextNode(child);
+                }
+                el.appendChild(child);
+            }
+            return el;
+        }
+    }
+});
+const el = dom.div({},
+    'Hello, my name is ',
+    dom.a({ href: '//example.com' }, 'Mark'),
+    '. I like:',
+    dom.ul({},
+        dom.li({}, 'The web'),
+        dom.li({}, 'Food'),
+        dom.li({}, '…actually that\'s it')
+    )
+);
+document.body.appendChild(el);
+// -------------------------------------------------------------
