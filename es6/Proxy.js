59d9930e1748eea4aa0ba713b8219df118dd4e79
AFODC-605221150\Administrator <chenyin151@163.com>
2018-04-03 02:09:35 +0800

    1.原对象不可配置或者禁止扩展，这时has拦截会报错 2.has只对in生效，对for...in循环不生效

--- es6/Proxy.js
+++ es6/Proxy.js
@@ -320,7 +320,7 @@ var twice = {
 //     在proxy中不管参数是以数组的形式还是参数列表的形式传入，
 //     apply都会把它转变为类数组的对象
     apply(target, ctx, args) {
-        debugger
+        
         // Reflect.apply(target,thisArgument,argumentsList)
         // target:目标函数
         // thisArgument:target函数调用时绑定的this对象
@@ -338,4 +338,55 @@ console.log('proxy8.call:',proxy8.call(null,5,6));
 console.log('proxy8.apply:',proxy8.apply(null,[7,8]));
 // Reflect.apply也会被拦截
 console.log('Reflect.apply:',Reflect.apply(proxy8,null,[9,9]));
+// ------------------------------------------------------------
+
+// 执行in会触发has拦截，这个key就是in之前的查询参数
+var handler = {
+    has(target,key) {
+        // 若查询参数前有_则返回false
+        if(key[0] === '_'){
+            return false;
+        }
+        // 检查这个key在target中是否存在
+        return key in target;
+    }
+};
+var target = {prop:'foo',prop:'foo'};
+var proxy9 = new Proxy(target,handler);
+console.log('_prop','prop' in proxy9);
+// ------------------------------------------------------------
+
+// 原对象不可配置或者禁止扩展，这时has拦截会报错
+var obj11 = {a:10};
+Object.preventExtensions(obj11);
+var p=new Proxy(obj11,{
+    has:function(target,prop){
+        return false;
+    }
+})
+console.log('a' in p);
+// ------------------------------------------------------------
+
+// has拦截对for...in循环不生效
+let stu1 = {name:'张三',score:59};
+let stu2 = {name:'李四',score:99};
+let handler = {
+    has(target,prop) {
+        if(prop === 'score' && target[prop]<60){
+            console.log(`${target.name}不及格`);
+            return false;
+        }
+        return prop in target;
+    }
+}
+let oproxy1 =new Proxy(stu1,handler);
+let oproxy2=new Proxy(stu2,handler);
+'score' in oproxy1;
+'score' in oproxy2;
+for(let a in oproxy1) {
+    console.log(oproxy1[a]);
+}
+for(let b in oproxy2){
+    console.log(oproxy2[b]);
+}
 // ------------------------------------------------------------
